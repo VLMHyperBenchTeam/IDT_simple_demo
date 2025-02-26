231989587057bef6e47b0b18679427d39451a981
Anton Shiryaev <med.phisiker@gmail.com>
2025-02-26 21:23:58 +1000

    feat(mvp): Реализовал MVP для Демо
    Реализовал MVP для Демо

--- pdf_mapper.py
+++ pdf_mapper.py
@@ -1,14 +1,10 @@
-import tempfile
-
-from pypdf import PdfWriter, PdfReader
+from pypdf import PdfWriter
 
 
 def pdf_to_mappings(pdf_in_path: str, mapping_list: list, output_file_path):
-  """The function takes PDF_in from path and saves its pages with correct order by mapping_list """
-  writer = PdfWriter()
-  writer.append(fileobj=pdf_in_path, pages=mapping_list)
+    """The function takes PDF_in from path and saves its pages with correct order by mapping_list"""
+    writer = PdfWriter()
+    writer.append(fileobj=pdf_in_path, pages=mapping_list)
 
-  writer.write(output_file_path)
-  writer.close()
-
-
+    writer.write(output_file_path)
+    writer.close()
